a7b215f48b3710514bd9f0253343e8f3d685ca40
Yakov Dalinchuk <yakov.dalinchuk@yandex.ru>
2017-11-20 12:47:55 +0000

    Initial commit

---
+++ dev-server.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config');
+var webpackDev = require('webpack-dev-middleware');
+var webpackHot = require('webpack-hot-middleware');
+
+var app = express();
+var compiler = webpack(config);
+
+app.use(webpackDev(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,
+}));
+
+app.use(webpackHot(compiler));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'examples/index.html'));
+});
+
+var port = process.env.PORT || 3000;
+var host = process.env.IP || 'localhost';
+
+var server = app.listen(port, host, () => {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server running at http(s)://' + host + ':' + port + '/\n'); // eslint-disable-line
+});
---
+++ examples/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Portal from '../lib/portal';
+
+const nodeFabric = () => document.createElement('section');
+const node = document.createElement('section');
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      portalIsOpen: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { portalIsOpen } = this.state;
+    this.setState({ portalIsOpen: ! portalIsOpen });
+  }
+
+  render() {
+    const { portalIsOpen } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click me!</button>
+
+        {portalIsOpen && (
+          <Portal>
+            <div>Base functionality example</div>
+          </Portal>
+        )}
+
+        <Portal isContentMount={portalIsOpen}>
+          <div>`isContentMount` attribute example</div>
+        </Portal>
+
+        <Portal portalName="portal-name">
+          <div>`portalName` attribute example</div>
+        </Portal>
+
+        <Portal node="section">
+          <div>`node` attribute example (tag name)</div>
+        </Portal>
+
+        <Portal node={nodeFabric}>
+          <div>`node` attribute example (HTMLElement)</div>
+        </Portal>
+
+        <Portal node={node}>
+          <div>`node` attribute example (HTMLElement fabric)</div>
+        </Portal>
+
+        <Portal className="some-class">
+          <div>`className` attribute example</div>
+        </Portal>
+
+        <Portal zIndex="1">
+          <div>`zIndex` attribute example</div>
+        </Portal>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
